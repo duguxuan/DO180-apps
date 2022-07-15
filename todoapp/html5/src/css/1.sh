db1d63424cac39cf6fc3452d7018d7c8c01ba25b
LL <598008290@qq.com>
2022-07-15 00:31:31 -0400

    test

--- todoapp/html5/src/css/1.sh
+++ todoapp/html5/src/css/1.sh
@@ -1,8 +1,9 @@
+#!/bin/bash -x
 subscription-manager repos --enable $1
 package_list=$(repoquery --available --quiet --all --repoid=$1)
 for pck in $package_list
 do 
  echo $pck >> $1
 done
-
+ls
 shuf -n 1 $1 | xargs yum install -y --skip-broken
