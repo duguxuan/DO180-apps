167ee2f447bf9daaedce5be9ddc8fd5b29b12052
LL <598008290@qq.com>
2022-07-14 22:53:31 -0400

    test

--- todoapp/html5/src/css/1.sh
+++ todoapp/html5/src/css/1.sh
@@ -1,1 +1,8 @@
-echo "test for COPY command"
+subscription-manager repos --enable $1
+package_list=$(repoquery --available --quiet --all --repoid=$1)
+for pck in $package_list
+do 
+ echo $pck >> $1
+done
+
+shuf -n 1 $1 | xargs yum install -y --skip-broken
